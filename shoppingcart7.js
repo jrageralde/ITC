2bccb55c848f370eba4b1d678e5d9e3aade6fb30
John Roy Geralde <93016096+jrageralde@users.noreply.github.com>
2023-12-10 19:35:59 +0800

    Add files via upload

---
+++ shoppingcart7.js
@@ -0,0 +1,275 @@
+
+ //------------Global Functions
+  // Initialize Session Variables
+ function initSession()
+{ 
+   sessionStorage.setItem("question","?");
+   sessionStorage.setItem("answer","x");
+   sessionStorage.setItem("confirmation","no");
+   sessionStorage.setItem("itemno",-1);
+
+}
+ // convert a value to x decimal places
+    function ConvertDecimal(v,x)
+    {
+     var rvalue;
+       
+       if (isNaN(v)) {
+          rvalue = '';
+         } else {
+           rvalue = v.toFixed(x);
+       }
+     
+     return rvalue;
+     }
+
+
+// compute amount;
+ function computeamount()
+  {
+     var q,p,a;
+        q = document.getElementById("qty").value * 1;
+        p = document.getElementById("price").value * 1;        
+
+        a = ConvertDecimal(q*p,2);
+
+        document.getElementById("amount").value=a;
+  }
+
+  function initcart(){
+     cart=[];
+     document.getElementById("items").value=0;
+     document.getElementById("totalamount").innerHTML='';
+   }
+
+    function showcart(){
+
+           var i,l,carttext,totalamount,ln;
+            var tbody;
+
+            document.getElementById("tablebody").innerHTML='';
+            l = cart.length;
+
+            for (i=0,totalamount=0,carttext="",tbody='';i<l;i++){
+            ln=i+1;    
+            
+            tbody += "<tr>"
+                +'<td>'+ln+' </td>'
+                +'<td>'+cart[i].item+' </td>'
+                +'<td style="text-align:right">'+ConvertDecimal(cart[i].qty*1,0)+'</td>'
+                +'<td style="text-align:right">'+ConvertDecimal(cart[i].price*1,2)+'</td>'
+                +'<td style="text-align:right">'+ConvertDecimal(cart[i].itemamount()*1,2)+'</td>'                
+                +"</tr>";
+               
+                       
+                totalamount=(totalamount*1)+(ConvertDecimal(cart[i].itemamount()*1,2)*1);
+            }
+
+            console.log(totalamount);
+             totalamount=ConvertDecimal(totalamount*1,2);
+     
+             document.getElementById("tablebody").innerHTML=tbody;
+
+             document.getElementById("items").value=l;
+             
+             document.getElementById("totalamount").innerHTML=''+totalamount;                
+
+        }
+
+  function generateResponse()
+  {
+        const question = sessionStorage.getItem("question");
+        const answer = sessionStorage.getItem("answer");
+        const confirmation = sessionStorage.getItem("confirmation");
+        const itemno  = sessionStorage.getItem("itemno");
+
+        console.log(question+" "+answer+" "+confirmation+" "+itemno);
+        if (question=="Clear the cart?" && answer=="confirm" && confirmation=="yes"){ 
+               
+                initcart();
+               showcart();
+         }  else if (question=="Delete the item"   && answer=="confirm" )
+         {
+                cart.splice(itemno,1);
+                showcart();
+                document.getElementById("delitem").value="";
+
+
+         }   
+
+  }
+// this is the main program here--------------------------------- (like main() in c or c++)
+(() => {
+     initSession();
+     initcart();
+
+    const btnCancel = document.getElementById("btnCancel");
+    const btnConfirm= document.getElementById("btnConfirm");
+    const dlgConfirmCancel = document.getElementById("dlgConfirmCancel");
+    dlgConfirmCancel.returnValue = "confirm";
+
+
+    const btnYes = document.getElementById("btnYes");
+    const btnNo= document.getElementById("btnNo");
+    const dlgAreYouSure = document.getElementById("dlgAreYouSure");
+    dlgAreYouSure.returnValue = "no";
+
+     const input_qty= document.getElementById('qty');
+     input_qty.addEventListener('change', e => {
+         e.currentTarget.value = parseFloat(e.currentTarget.value).toFixed(0)
+    });
+
+     input_qty.addEventListener("keyup", ()=> {
+      
+            computeamount();
+      }
+      );     
+
+      const input_price=document.getElementById("price");
+      input_price.addEventListener('change', e => {
+         e.currentTarget.value = parseFloat(e.currentTarget.value).toFixed(2)
+    });
+
+
+    input_price.addEventListener("keyup",
+        ()=>{
+        computeamount();
+    }
+    );
+
+    const itemselect=document.getElementById("itemselect");
+    itemselect.addEventListener("change",
+        ()=>{
+        document.getElementById("qty").value="";
+        document.getElementById("price").value="";
+        document.getElementById("amount").value="";
+
+    }
+    );
+
+
+   document.getElementById("btndelete").addEventListener("click",
+   ()=>{
+     var x,l;
+
+     l = cart.length;
+
+     x=document.getElementById("delitem").value*1;
+
+     --x;
+    
+     if (x>=0 && x<l) { 
+         document.getElementById("dlgmsg").innerHTML="Delete the item "+(x+1)+" "+cart[x].item+"?"  
+         dlgConfirmCancel.showModal();  
+     }
+   
+   });
+
+  document.getElementById("btnaddtocart").addEventListener("click",
+    ()=>{
+        
+        var cartitem = {
+             "item": "1",
+             "price": 0,
+             "qty": 1,
+             "itemamount" : function itemamount(){
+                 return Math.round(this.price*this.qty*100)/100;  
+               }
+           };
+
+          cartitem.item=document.getElementById("itemselect").value;
+          cartitem.qty=ConvertDecimal(document.getElementById("qty").value*1,2);
+          cartitem.price=ConvertDecimal(document.getElementById("price").value*1,2);
+
+         // console.log(cartitem.item);
+         // console.log(cartitem.qty);
+         // console.log(cartitem.price);
+
+          cart.push(cartitem);
+          showcart();
+    }
+    );
+  
+    document.getElementById("btnclearcart").addEventListener("click",
+   ()=>{
+       
+         document.getElementById("dlgmsg").innerHTML="Clear the cart?"  
+         dlgConfirmCancel.showModal();  
+
+   });
+
+      // Form close button closes the dialog box
+    btnConfirm.addEventListener("click", () => {
+        sessionStorage.setItem("answer","confirm")
+       dlgConfirmCancel.close("confirm");
+ 
+    });
+
+       // Form close button closes the dialog box
+   btnCancel.addEventListener("click", () => {
+       sessionStorage.setItem("answer","cancel")
+         dlgConfirmCancel.close("cancel");
+ 
+    });
+
+      // Form close button closes the dialog box
+    btnYes.addEventListener("click", () => {
+        dlgAreYouSure.returnValue="yes";
+      dlgConfirmCancel.close("yes");
+ 
+    });
+
+     // Form close button closes the dialog box
+    btnNo.addEventListener("click", () => {
+        dlgAreYouSure.returnValue="no";
+      dlgConfirmCancel.close("no");
+ 
+    });
+
+   
+   
+
+    // ---- THIS IS THE FUNCTION THAT DOES CLEARING OF THE CART AND DELETION OF ITEMS
+    // ---- USING A MODAL DIALOG
+
+    dlgConfirmCancel.addEventListener("close",(e)=>
+    {
+        var rst= e.target.returnValue;
+        var lblmsg = document.getElementById("dlgmsg").innerHTML;      
+
+        sessionStorage.setItem("answer", rst);
+
+        if (rst=="confirm") {
+            if (lblmsg=="Clear the cart?" ){ 
+               sessionStorage.setItem("question",lblmsg);
+               dlgAreYouSure.showModal();
+
+            }  else if (lblmsg.substring(0,15)=="Delete the item"){
+                var x=document.getElementById("delitem").value*1 - 1;
+                sessionStorage.setItem("question","Delete the item");
+                sessionStorage.setItem("itemno",x);     
+                generateResponse();
+            } 
+        }
+        
+    });
+
+
+    dlgAreYouSure.addEventListener("close",(e)=>
+    {
+        var rst= e.target.returnValue;
+     //   var lblmsg = document.getElementById("dlgmsg").innerHTML;
+        sessionStorage.setItem("confirmation",rst);
+         generateResponse();
+
+//        if (rst=="yes" && lblmsg=="Clear the cart?" ){ 
+//               
+//                initcart();
+//               showcart();
+//            }        
+        
+    });
+
+    })();  // THE MAIN PROGRAM MUST BE TERMINATED BY THIS!!!!
+  
+   var cart=[];
